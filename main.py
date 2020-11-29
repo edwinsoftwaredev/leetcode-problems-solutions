02e8a0f7e2a2cdf61aa0f8a9b755f1a45ff833a0
Edwin <edwin_m_paz@hotmail.com>
2020-11-28 18:05:14 -0600

    Add main.py file

---
+++ main.py
@@ -0,0 +1,27 @@
+"""
+module documentation
+"""
+# This is a sample Python script.
+from longest_substring_unrepeated_chars import LongestSubstringUnrepeatedChars
+
+# Press Mayús+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    """
+    Print 'Hi, + name'
+    :param name: a name
+    :return: None
+    """
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('Edwin')
+    x = LongestSubstringUnrepeatedChars()
+    print(x.length_of_longest_substring('xyzaab'))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
